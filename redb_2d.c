f3c13d7ca9f4ea7f7614d57ad22cf7657f00e4ad
MariaMsu <i.knaz@yandex.ru>
2020-11-02 20:36:14 +0300

    fix codestyle

--- redb_2d.c
+++ redb_2d.c
@@ -1,86 +1,78 @@
 #include <math.h>
 #include <stdlib.h>
 #include <stdio.h>
-#define  Max(a,b) ((a)>(b)?(a):(b))
+
+#define  Max(a, b) ((a)>(b)?(a):(b))
 
 #define  N   (2*2*2*2*2*2+2)
-float   maxeps = 0.1e-7;
+float maxeps = 0.1e-7;
 int itmax = 100;
-int i,j,k;
+int i, j, k;
 float w = 0.5;
 float eps;
 
-float A [N][N];
+float A[N][N];
 
 void relax();
+
 void init();
-void verify(); 
 
-int main(int an, char **as)
-{
-	int it;
-	init();
+void verify();
 
-	for(it=1; it<=itmax; it++)
-	{
-		eps = 0.;
-		relax();
-		printf( "it=%4i   eps=%f\n", it,eps);
-		if (eps < maxeps) break;
-	}
+int main(int an, char **as) {
+    int it;
+    init();
 
-	verify();
+    for (it = 1; it <= itmax; it++) {
+        eps = 0.;
+        relax();
+        printf("it=%4i   eps=%f\n", it, eps);
+        if (eps < maxeps) break;
+    }
 
-	return 0;
+    verify();
+
+    return 0;
 }
 
 
-void init()
-{ 
-	for(j=0; j<=N-1; j++)
-	for(i=0; i<=N-1; i++)
-	{
-		if(i==0 || i==N-1 || j==0 || j==N-1) A[i][j]= 0.;
-		else A[i][j]= ( 1. + i + j) ;
-	}
-} 
+void init() {
+    for (j = 0; j <= N - 1; j++)
+        for (i = 0; i <= N - 1; i++) {
+            if (i == 0 || i == N - 1 || j == 0 || j == N - 1) A[i][j] = 0.;
+            else A[i][j] = (1. + i + j);
+        }
+}
 
 
-void relax()
-{
+void relax() {
 
-	for(j=1; j<=N-2; j++)
-	for(i=1; i<=N-2; i++)
-	if ((i + j) % 2 == 1)
-	{
-		float b;
-		b = w*((A[i-1][j]+A[i+1][j]+A[i][j-1]+A[i][j+1])/4. - A[i][j]);
-		eps =  Max(fabs(b),eps);
-		A[i][j] = A[i][j] + b;
-	}
-	for(j=1; j<=N-2; j++)
-	for(i=1; i<=N-2; i++)
-	if ((i + j) % 2 == 0)
-	{
-		float b;
-		b = w*((A[i-1][j]+A[i+1][j]+A[i][j-1]+A[i][j+1])/4. - A[i][j]);
-		A[i][j] = A[i][j] + b;
-	}
+    for (j = 1; j <= N - 2; j++)
+        for (i = 1; i <= N - 2; i++)
+            if ((i + j) % 2 == 1) {
+                float b;
+                b = w * ((A[i - 1][j] + A[i + 1][j] + A[i][j - 1] + A[i][j + 1]) / 4. - A[i][j]);
+                eps = Max(fabs(b), eps);
+                A[i][j] = A[i][j] + b;
+            }
+    for (j = 1; j <= N - 2; j++)
+        for (i = 1; i <= N - 2; i++)
+            if ((i + j) % 2 == 0) {
+                float b;
+                b = w * ((A[i - 1][j] + A[i + 1][j] + A[i][j - 1] + A[i][j + 1]) / 4. - A[i][j]);
+                A[i][j] = A[i][j] + b;
+            }
 
 }
 
 
-void verify()
-{ 
-	float s;
+void verify() {
+    float s;
 
-	s=0.;
-	for(j=0; j<=N-1; j++)
-	for(i=0; i<=N-1; i++)
-	{
-		s=s+A[i][j]*(i+1)*(j+1)/(N*N);
-	}
-	printf("  S = %f\n",s);
+    s = 0.;
+    for (j = 0; j <= N - 1; j++)
+        for (i = 0; i <= N - 1; i++) {
+            s = s + A[i][j] * (i + 1) * (j + 1) / (N * N);
+        }
+    printf("  S = %f\n", s);
 }
-
-
